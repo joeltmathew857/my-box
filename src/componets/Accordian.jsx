6ac39b0c45a30b961a596008477fd04a2df219b0
joel thomas mathew <joelmathew857@gmail.com>
2023-08-28 11:46:41 +0530

    added props

---
+++ src/componets/Accordian.jsx
@@ -0,0 +1,27 @@
+// Support.jsx
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+
+const Accordian = ({
+  title = "",
+  description = "",
+  isOpen = false,
+  onToggle = () => {},
+}) => {
+  return (
+    <div>
+      <div className="cursor-pointer border-b-4 border-zinc-950 py-4">
+        {title}
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          onClick={() => onToggle}
+          className={`${isOpen ? "rotate-180" : ""} w-12 h-6 text-yellow-500`}
+        />
+        {isOpen && <div className="drop">{description}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
--- src/componets/FAQcard.jsx
+++ src/componets/FAQcard.jsx
@@ -1,46 +1,56 @@
 import React, { useState } from "react";
+import Accordian from "./Accordian";
 import FAQimg from "../assets/FAQimg/illustration-woman-online-mobile.svg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const FAQcard = () => {
-  const [dropdowns, setDropdowns] = useState(false);
+  const [dropdowns, setDropdowns] = useState({});
 
-  const toggleDropdown = (dropdownName) => {
+  const toggleDropdown = (accordianName) => {
     setDropdowns((prevDropdowns) => ({
       ...prevDropdowns,
-      [dropdownName]: !prevDropdowns[dropdownName],
+      [accordianName]: !prevDropdowns[accordianName],
     }));
   };
 
   return (
     <div className="flex justify-center h-screen items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-      <div className="max-w-sm mx-auto bg-white sm:max-w-md md:max-w-lg flex justify-between flex flex-row lg:max-w-xl xl:max-w-3xl rounded-2xl shadow-md text-center">
+      <div className="mx-auto bg-white sm:max-w-md md:max-w-lg flex justify-between flex flex-row lg:max-w-xl xl:max-w-3xl rounded-2xl shadow-md text-center">
         <div>
           <img src={FAQimg} alt="FAQimg" className="w-96 h-96" />
         </div>
-        <div className="drop-down p-6 ">
+        <div className="drop-down p-6">
           <div className="font-bold text-4xl pb-10">FAQ</div>
-          <div
-            className="cursor-pointer underline py-4"
-            onClick={() => toggleDropdown("teamMembers")}
-          >
-            How many team members can I invite?
-            <FontAwesomeIcon
-              icon={faChevronDown}
-              className={dropdowns.teamMembers ? "rotate-180" : ""}
-            />
-          </div>
-          {dropdowns.teamMembers && (
-            <div className="drop">
-              You can invite up to 2 additional users on the Free plan. There is
-              no limit on team members for the Premium plan.
-            </div>
-          )}
-          {/* Repeat the above pattern for other questions */}
-          {/* ...
-          ...
-          ... */}
+          <Accordian
+            title="How many team members can I invite?"
+            description="You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan."
+            isOpen={dropdowns.invite}
+            onToggle={() => toggleDropdown("invite")}
+          />
+          <Accordian
+            title="   Do you provide additional support?            "
+            description="  Chat and email support is available 24/7. Phone lines are open during normal business hours.            "
+            isOpen={dropdowns.support}
+            onToggle={() => toggleDropdown("support")}
+          />
+          <Accordian
+            title=" What is the maximum file upload size?"
+            description="No more than 2GB. All files in your account must fit your allotted storage space"
+            isOpen={dropdowns.Upload}
+            onToggle={() => toggleDropdown("Upload")}
+          />
+          <Accordian
+            title="  How do I reset my password?"
+            description=" Click “Forgot password” from the login page or “Change password”
+            from your profile page. A reset link will be emailed to you."
+            isOpen={dropdowns.password}
+            onToggle={() => toggleDropdown("password")}
+          />
+          <Accordian
+            title="Can I cancel my subscription?"
+            description=" Yes! Send us a message and we’ll process your request no questions asked."
+            isOpen={dropdowns.subscribe}
+            onToggle={() => toggleDropdown("subscribe")}
+          />
         </div>
       </div>
     </div>
