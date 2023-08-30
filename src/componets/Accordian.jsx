48d83c880235562bbae120e99cd3e66029234749
joel thomas mathew <joelmathew857@gmail.com>
2023-08-30 09:48:44 +0530

    add Responsive-Design

--- src/componets/Accordian.jsx
+++ src/componets/Accordian.jsx
@@ -1,4 +1,3 @@
-// Support.jsx
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -10,18 +9,20 @@ const Accordian = ({
   onToggle = () => {},
 }) => {
   return (
-    <div className="border-b-2 border-zinc-200">
+    <div className="border-b-2 border-zinc-200 ">
       <div
-        className={`cursor-pointer text-md  py-3 flex flex-row px-12 text-md justify-between ${
+        className={`cursor-pointer sm:w-26 py-3 flex flex-row px-12 text-md justify-between ${
           isOpen ? "font-bold" : "font-light"
         }`}
       >
         <div className="flex whitespace-nowrap text-slate-600 ">{title}</div>
-        <FontAwesomeIcon
-          icon={faChevronDown}
-          onClick={() => onToggle()}
-          className={`${isOpen ? "rotate-180" : ""} w-4 h-4 text-yellow-500 `}
-        />
+        <div className="">
+          <FontAwesomeIcon
+            icon={faChevronDown}
+            onClick={() => onToggle()}
+            className={`${isOpen ? "rotate-180" : ""} w-4 h-4 text-yellow-500 `}
+          />
+        </div>
       </div>
       {isOpen && <div className="drop text-left">{description}</div>}
     </div>
